ffc3ef4dce798dfcc13df6d323464cff121eb722
Katie Spivey <katieaspivey@gmail.com>
2025-03-28 10:11:52 -0400

    player placeholder moving

--- app.py
+++ app.py
@@ -19,19 +19,33 @@ player_rect = pygame.Rect(30, 30, 30, 30)
 class Player():
     def __init__(self, x, y, rect):
         self.x = x
-        self.y = y
-        self.rect = rect
+        self.y = y 
+        self.image = pygame.image.load('images/placeholder-player.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
 
     def update(self):
-
+        dx = 0
+        dy = 0
+        
+        
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-           self.x -= 5
+           dx -= 10
         if key[pygame.K_RIGHT]:
-           self.x += 5
+           dx += 10
+        if key[pygame.K_DOWN]:
+            dy += 10
+        if key[pygame.K_UP]:
+            dy -= 10
 
-        self.rect = pygame.draw.rect(screen, RED, player_rect)
-        screen.blit(screen, self.rect)
+        self.rect.x += dx
+        self.rect.y += dy
+
+        screen.blit(self.image, self.rect)
         
         pygame.display.flip()
     
@@ -45,10 +59,8 @@ def gameLoop():
 
     while is_running:
 
-        screen.fill(BLACK)
-        pygame.display.flip()
+        screen.fill(WHITE)
         clock.tick(FPS)
-        
 
         for event in pygame.event.get():
 
@@ -59,6 +71,6 @@ def gameLoop():
                 print("done")
                 break
 
-        pygame.display.flip()
+        # pygame.display.flip()
 
 gameLoop()
