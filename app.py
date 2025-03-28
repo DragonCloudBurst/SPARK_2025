984e6a8c0aa1a7dd25ddccf8d6190d9fa069b459
Katie Spivey <katieaspivey@gmail.com>
2025-03-28 09:57:44 -0400

    setup for player movement

--- app.py
+++ app.py
@@ -14,20 +14,51 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Spark 2025")
 
+player_rect = pygame.Rect(30, 30, 30, 30)
+
+class Player():
+    def __init__(self, x, y, rect):
+        self.x = x
+        self.y = y
+        self.rect = rect
+
+    def update(self):
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+           self.x -= 5
+        if key[pygame.K_RIGHT]:
+           self.x += 5
+
+        self.rect = pygame.draw.rect(screen, RED, player_rect)
+        screen.blit(screen, self.rect)
+        
+        pygame.display.flip()
+    
+
 def gameLoop():
     is_running = True
     clock = pygame.time.Clock()
     clock.tick(FPS)
 
+    player = Player(300, 200, player_rect)
+
     while is_running:
 
-        screen.fill(RED)
+        screen.fill(BLACK)
         pygame.display.flip()
         clock.tick(FPS)
+        
 
         for event in pygame.event.get():
+
+            player.update()
+
             if event.type == pygame.QUIT:
                 is_running = False
                 print("done")
-                pygame.quit()
                 break
+
+        pygame.display.flip()
+
+gameLoop()
