8a2946017e58d21bee91ec5402db7b2de67c7566
Katie Spivey <katieaspivey@gmail.com>
2025-03-28 09:31:41 -0400

    boilerplate

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
 import pygame
 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+FPS = 30
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
 pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Spark 2025")
+
+def gameLoop():
+    is_running = True
+    clock = pygame.time.Clock()
+    clock.tick(FPS)
+
+    while is_running:
+
+        screen.fill(RED)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+                print("done")
+                pygame.quit()
+                break
