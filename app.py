35007460faacfe543e3e91306302f076ba1717a3
Ja'Quis <jaquismay2004@gmail.com>
2025-03-28 10:52:40 -0400

    Collision

--- app.py
+++ app.py
@@ -17,8 +17,12 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Spark 2025")
 
 player_rect = pygame.Rect(30, 30, 30, 30)
+enemy_rect = pygame.Rect(40, 40, 40, 40)
 
-class Player():
+'''
+This is the class for an enemy in the game
+'''
+class Opp():
     def __init__(self, x, y, rect):
         self.x = x
         self.y = y 
@@ -30,43 +34,71 @@ class Player():
         self.height = self.image.get_height()
 
     def update(self):
-        dx = 0
-        dy = 0
-        
-        key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT]:
-           dx -= 10
-        if key[pygame.K_RIGHT]:
-           dx += 10
-        if key[pygame.K_DOWN]:
-            dy += 10
-        if key[pygame.K_UP]:
-            dy -= 10
-
-        self.rect.x += dx
-        self.rect.y += dy
-
         screen.blit(self.image, self.rect)
-        
         pygame.display.flip()
+
+
+
+class Player():
+    def __init__(self, x, y, rect):
+        self.x = x
+        self.y = y 
+        self.image = pygame.image.load('images/placeholder-player.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.moving = True
+
+    def update(self):
+        if self.moving:
+            dx = 0
+            dy = 0
+            
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT]:
+                dx -= 10
+            if key[pygame.K_RIGHT]:
+                dx += 10
+            if key[pygame.K_DOWN]:
+                dy += 10
+            if key[pygame.K_UP]:
+                    dy -= 10
+
+            self.rect.x += dx
+            self.rect.y += dy
+
+            screen.blit(self.image, self.rect)
+            
+            pygame.display.flip()
     
+
+
 
 def gameLoop():
     is_running = True
     clock = pygame.time.Clock()
     clock.tick(FPS)
 
-    player = Player(300, 200, player_rect)
+    player = Player(300, 200, player_rect) 
+    enemy = Opp(100, 100, enemy_rect)
+
+
 
     while is_running:
 
         screen.fill(WHITE)
         clock.tick(FPS)
+        
+        if player.rect.colliderect(enemy.rect) :
+            player.moving = False
 
         for event in pygame.event.get():
 
             player.update()
-
+            enemy.update()
+            
             if event.type == pygame.QUIT:
                 is_running = False
                 print("done")
